1bb9920e694752874f236fc3ff6d577fe0cb7876
MarloninoRG <marlonrojasuniversity@gmail.com>
2025-09-29 17:39:52 -0600

    Update Electronico.java
    Correccion de errores

--- actividadTienda/Electronico.java
+++ actividadTienda/Electronico.java
@@ -1,41 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package poo.actividadTienda;
-
 /**
- *
- * @author Marlon Rojas Galindo
+ * Clase Electronico que hereda de Producto<String>
  */
-public class Electronico extends Producto {
-    private String garantia;
+public class Electronico extends Producto<String> {
 
-    public Electronico(String garantia, String nombre, float precio, Object informacion) {
-        super(nombre, precio, informacion);
-        this.garantia = garantia;
-    }
-
-    public Electronico() {
-    }
-
-    public Electronico(String garantia) {
-        this.garantia = garantia;
-    }
-
-    public String getGarantia() {
-        return garantia;
-    }
-
-    public void setGarantia(String garantia) {
-        this.garantia = garantia;
+    /**
+     * Constructor de la clase Electronico
+     * @param nombre Nombre del producto
+     * @param precio Precio del producto
+     * @param garantia Tiempo de garantía
+     */
+    public Electronico(String nombre, double precio, String garantia) {
+        super(nombre, precio, garantia);
     }
 
     @Override
     public void mostrarDetalles() {
-        System.out.println("Nombre: " + super.getNombre());
-        System.out.println("Precio: " + super.getPrecio());
-        System.out.println("Garantia: " + this.getGarantia());
+        System.out.println("Electrónico: " + getNombre() + 
+                           " | Precio: $" + getPrecio() + 
+                           " | Garantía: " + getExtra());
     }
-    
 }
